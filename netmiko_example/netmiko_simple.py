f13b437b49f3872787f221ff9b85d6c70503c8a5
Kirk Byers <ktbyers@twb-tech.com>
2017-10-16 14:30:37 -0700

    Correct device reference

--- netmiko_example/netmiko_simple.py
+++ netmiko_example/netmiko_simple.py
@@ -6,12 +6,12 @@ from netmiko import ConnectHandler
 if __name__ == "__main__":
 
     password = getpass("Enter password: ")
-    srx = {
+    cisco1 = {
         'device_type': 'cisco_ios',
         'ip':   '184.105.247.70',
         'username': 'pyclass',
         'password': password
     }
 
-    net_connect = ConnectHandler(**srx)
+    net_connect = ConnectHandler(**cisco1)
     print(net_connect.find_prompt())
