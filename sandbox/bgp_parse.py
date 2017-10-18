8925ec79dff904a469b9e6168bbed27458f61470
Kirk Byers <ktbyers@twb-tech.com>
2017-10-18 15:02:18 -0700

    Travis CI

--- sandbox/bgp_parse.py
+++ sandbox/bgp_parse.py
@@ -1,17 +1,23 @@
+"""Parsing bgp output getting prefix and as_path."""
 from __future__ import print_function
 
-filename = "show_ip_bgp.txt"
-f = open(filename)
+def main():
+    filename = "show_ip_bgp.txt"
+    f = open(filename)
 
-show_bgp = f.read()
+    show_bgp = f.read()
 
-fields = show_bgp.split("Weight Path")
-bgp_table = fields[1]
-bgp_table = bgp_table.strip()
+    fields = show_bgp.split("Weight Path")
+    bgp_table = fields[1]
+    bgp_table = bgp_table.strip()
 
-for line in bgp_table.splitlines():
-    line = line.strip()
-    fields = line.split()
-    prefix = fields[1]
-    as_path = fields[5:-1]
-    print("Prefix: {}, AS_Path: {}".format(prefix, as_path))
+    for line in bgp_table.splitlines():
+        line = line.strip()
+        fields = line.split()
+        prefix = fields[1]
+        as_path = fields[5:-1]
+        print("Prefix: {}, AS_Path: {}".format(prefix, as_path))
+
+
+if __name__ == "__main__":
+    main()
